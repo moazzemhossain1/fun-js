eed4f20015edce39819fa1427029c0278897e10a
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-23 07:00:57 +0800

    using argement

---
+++ agument.js
@@ -0,0 +1,31 @@
+function multiple(a,b){
+    let mul=a*b;
+    console.log("The mul result is =",mul)
+}
+multiple(7,3);
+multiple(7,8)
+
+
+function squer(x){
+    let z=x*x;
+    console.log("The squer is =",z)
+}
+let mony=3;
+squer(mony)
+
+
+function division(c,d){
+    let e=c/d;
+    console.log("The division result is ",e)
+
+}
+division(30,5)
+
+
+
+
+function modules(y,f){
+    let i=y%f;
+    console.log("The modules is ", i)
+}
+modules(5,4)
