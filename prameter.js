25d5699523f6c60eb5150115b61d65d1501739d0
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-23 06:25:46 +0800

    using prameter

---
+++ prameter.js
@@ -0,0 +1,36 @@
+function user(name){
+    console.log("Hi!,", name);
+}
+user("Kariem");
+user("Rahime");
+user("Jober");
+
+
+function add(a,b){
+    let c=a+b;
+    console.log(a+b);
+}
+add(3,5);
+add("The totle number is:",34,32);
+
+
+function sub(x,y){
+
+    let z=x-y;
+    console.log(z);
+}
+// sub("The totle number is:",34,32);
+
+// sub("the totle sub is:",30,20);
+// sub("The totle sub is: ", 90,50);
+sub(34,30)
+
+
+
+
+function mul(a,b){
+    let d=a*b;
+    console.log(d)
+}
+mul("the mul is :",3,6)
+mul(4,4)
