4fdb31e26a915c294bde473d0909dcb41eb21783
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-23 07:32:18 +0800

    using return

---
+++ return.js
@@ -0,0 +1,34 @@
+function sum(a,b){
+    let z=a+b;
+    return z;
+}
+let totle=sum(2,3);
+console.log("The totle value is ",totle);
+
+
+
+function sub(num1,num2){
+    const num3=num1-num2;
+    return num3
+}
+const totl=sub(40,30);
+console.log("The sub is ",totl)
+
+
+
+function multiple(x,y){
+    const z=x*y;
+    return z;
+
+}
+const mul=multiple(3,5);
+console.log("The totle multiple is " , mul)
+
+
+
+function division(s,y){
+    const i=s/y;
+    return i;
+}
+const div=division(5,4);
+console.log("The totle division is ",div)
