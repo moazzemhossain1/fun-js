41a1a7b2df4fa07b088c0d341ed6cb72265175f6
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-23 08:15:04 +0800

    odd or even

---
+++ isEven.js
@@ -0,0 +1,25 @@
+function even(a){
+    if(a%2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const result=even(8);
+console.log(result)
+
+
+
+
+function odd(x){
+    if(x%2===1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const od=odd(9);
+console.log(od)
